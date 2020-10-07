2d190adc8725c0644250d938d07f9445e46c8c2d
Gladys Dwi <gladysdeem2@gmail.com>
2020-10-08 01:02:07 +0700

    add logo

--- app/src/main/java/com/example/movieapp/SplashScreen.java
+++ app/src/main/java/com/example/movieapp/SplashScreen.java
@@ -2,6 +2,7 @@ package com.example.movieapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -22,7 +23,7 @@ public class SplashScreen extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(getApplicationContext(), WelcomeActivity.class));
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
                 finish();
             }
         }, 3000L); //3000 L = 3 detik
