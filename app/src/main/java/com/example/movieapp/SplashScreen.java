e0bcc5c47257ed50cc5b1669b308b90a63050b92
Renita Safitri <renitasafitri883@gmail.com>
2020-11-10 19:01:45 +0700

    Toolbar halaman home

--- app/src/main/java/com/example/movieapp/SplashScreen.java
+++ app/src/main/java/com/example/movieapp/SplashScreen.java
@@ -1,16 +1,12 @@
 package com.example.movieapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.Window;
 
-import com.example.movieapp.MainActivity;
-import com.example.movieapp.R;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
